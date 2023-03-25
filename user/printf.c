6dc2af7e1705d0fc5fbcc3e392f469acf786cce3
Khalid Bin Walid <kbwalid26@gmail.com>
2023-03-25 10:37:34 +0900

    rewrite printf using switch

--- user/printf.c
+++ user/printf.c
@@ -47,7 +47,7 @@ printptr(int fd, uint64 x) {
     putc(fd, digits[x >> (sizeof(uint64) * 8 - 4)]);
 }
 
-// Print to the given fd. Only understands %d, %x, %p, %s.
+// Print to the given fd. Only understands %d, %l, %x, %p, %s, %c, %%.
 void
 vprintf(int fd, const char *fmt, va_list ap)
 {
@@ -57,22 +57,29 @@ vprintf(int fd, const char *fmt, va_list ap)
   state = 0;
   for(i = 0; fmt[i]; i++){
     c = fmt[i] & 0xff;
-    if(state == 0){
+    switch (state) {
+    case 0:
       if(c == '%'){
         state = '%';
       } else {
         putc(fd, c);
       }
-    } else if(state == '%'){
-      if(c == 'd'){
+      break;
+    case '%':
+      switch (c) {
+      case 'd':
         printint(fd, va_arg(ap, int), 10, 1);
-      } else if(c == 'l') {
+        break;
+      case 'l':
         printint(fd, va_arg(ap, uint64), 10, 0);
-      } else if(c == 'x') {
+        break;
+      case 'x':
         printint(fd, va_arg(ap, int), 16, 0);
-      } else if(c == 'p') {
+        break;
+      case 'p':
         printptr(fd, va_arg(ap, uint64));
-      } else if(c == 's'){
+        break;
+      case 's':
         s = va_arg(ap, char*);
         if(s == 0)
           s = "(null)";
@@ -80,16 +87,20 @@ vprintf(int fd, const char *fmt, va_list ap)
           putc(fd, *s);
           s++;
         }
-      } else if(c == 'c'){
+        break;
+      case 'c':
         putc(fd, va_arg(ap, uint));
-      } else if(c == '%'){
+        break;
+      case '%':
         putc(fd, c);
-      } else {
+        break;
+      default:
         // Unknown % sequence.  Print it to draw attention.
         putc(fd, '%');
         putc(fd, c);
       }
       state = 0;
+      break;
     }
   }
 }
